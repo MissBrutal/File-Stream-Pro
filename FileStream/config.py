e2220124f650d9a348e81ba655c10db534a447db
Noob Developer <157978295+Geekyowner@users.noreply.github.com>
2024-07-18 20:15:47 +0530

    Update config.py

--- FileStream/config.py
+++ FileStream/config.py
@@ -26,16 +26,15 @@ class Telegram:
     SECONDARY = True if MODE.lower() == "secondary" else False
     AUTH_USERS = list(set(int(x) for x in str(env.get("AUTH_USERS", "")).split()))
 
-class Server:
+    class Server:
     PORT = int(env.get("PORT", 8080))
-    BIND_ADDRESS = str(env.get("BIND_ADDRESS", ""))
+    BIND_ADDRESS = str(env.get("BIND_ADDRESS", "0.0.0.0"))
     PING_INTERVAL = int(env.get("PING_INTERVAL", "1200"))
     HAS_SSL = str(env.get("HAS_SSL", "0").lower()) in ("1", "true", "t", "yes", "y")
-    NO_PORT = str(env.get("NO_PORT", "0").lower()) in ("1", "true", "t", "yes", "y")
+    NO_PORT = True  # Set this to True to exclude the port number in the URL
     FQDN = str(env.get("FQDN", BIND_ADDRESS))
-    URL = "http{}://{}{}/".format(
-        "s" if HAS_SSL else "", FQDN, "" if NO_PORT else ":" + str(PORT)
+    URL = "http{}://{}/".format(
+        "s" if HAS_SSL else "", FQDN
+    ) if NO_PORT else "http{}://{}:{}/".format(
+        "s" if HAS_SSL else "", FQDN, PORT
     )
-
-
-
