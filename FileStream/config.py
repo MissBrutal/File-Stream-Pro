f76543f8886c58b26e1acf68440f96746a88677c
Noob Developer <157978295+Geekyowner@users.noreply.github.com>
2024-07-18 20:18:37 +0530

    Update config.py

--- FileStream/config.py
+++ FileStream/config.py
@@ -27,13 +27,13 @@ class Telegram:
     AUTH_USERS = list(set(int(x) for x in str(env.get("AUTH_USERS", "")).split()))
 
     class Server:
-    PORT = int(env.get("PORT", 8080))
-    BIND_ADDRESS = str(env.get("BIND_ADDRESS", "0.0.0.0"))
-    PING_INTERVAL = int(env.get("PING_INTERVAL", "1200"))
-    HAS_SSL = str(env.get("HAS_SSL", "0").lower()) in ("1", "true", "t", "yes", "y")
-    NO_PORT = True  # Set this to True to exclude the port number in the URL
-    FQDN = str(env.get("FQDN", BIND_ADDRESS))
-    URL = "http{}://{}/".format(
+        PORT = int(env.get("PORT", 8080))
+        BIND_ADDRESS = str(env.get("BIND_ADDRESS", "0.0.0.0"))
+        PING_INTERVAL = int(env.get("PING_INTERVAL", "1200"))
+        HAS_SSL = str(env.get("HAS_SSL", "0").lower()) in ("1", "true", "t", "yes", "y")
+        NO_PORT = True  # Set this to True to exclude the port number in the URL
+        FQDN = str(env.get("FQDN", BIND_ADDRESS))
+        URL = "http{}://{}/".format(
         "s" if HAS_SSL else "", FQDN
     ) if NO_PORT else "http{}://{}:{}/".format(
         "s" if HAS_SSL else "", FQDN, PORT
