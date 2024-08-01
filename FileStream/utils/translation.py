dfbc6bd6062566aedb3f17fb7a248d061bfbc544
Noob Developer <157978295+Geekyowner@users.noreply.github.com>
2024-08-01 23:18:13 +0530

    Add files via upload

---
+++ FileStream/utils/translation.py
@@ -0,0 +1,88 @@
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from FileStream.config import Telegram
+
+class LANG(object):
+
+    START_TEXT = """
+<b>👋 Hᴇʏ, </b>{}\n
+<b> ɪ ᴀᴍ ᴀ ʟᴀᴛᴇꜱᴛ ꜰɪʟᴇ ꜱᴛʀᴇᴀᴍ ʙᴏᴛ ᴡɪᴛʜ ᴍᴀɴʏ ᴘʟᴀʏᴇʀ ꜱᴜᴘᴘᴏʀᴛ.
+
+● sᴇɴᴅ ᴍᴇ ᴀɴʏ ᴛᴇʟᴇɢʀᴀᴍ ᴠɪᴅᴇᴏ/ғɪʟᴇ/ᴅᴏᴄᴜᴍᴇɴᴛs ᴇᴛᴄ.. ᴛᴏ sᴇᴇ ᴍʏ ᴘᴏᴡᴇʀ.
+● ɪ ᴄᴀɴ ɢᴇɴᴇʀᴀᴛᴇ ꜱᴛʀᴇᴀᴍɪɴɢ ʟɪɴᴋ ᴏꜰ ʏᴏᴜʀ ᴠɪᴅᴇᴏ/ꜰɪʟᴇ ᴇᴛᴄ.
+● ɪ ᴄᴀɴ ɢᴇɴᴇʀᴀᴛᴇ ʜɪɢʜ sᴘᴇᴇᴅ ᴅᴏᴡɴʟᴏᴀᴅ ʟɪɴᴋ 
+
+❗Nᴏᴛᴇ: Gᴇɴᴇʀᴀᴛɪɴɢ Lɪɴᴋ Oғ Aᴅᴜʟᴛ Cᴏɴᴛᴇɴᴛ ɪs Sᴛʀɪᴄᴛʟʏ Pʀᴏʜɪʙɪᴛᴇᴅ 🚫
+ </b>"""
+
+    HELP_TEXT = """
+    <b>Rᴇᴀᴅ Mʏ Fᴇᴀᴛᴜʀᴇs 📝
+    
+➥ ᴀᴅᴅ ᴍᴇ ᴀꜱ ᴀᴅᴍɪɴ ɪɴ ʏᴏᴜʀ ᴄʜᴀɴɴᴇʟ ɪ'ʟʟ ɢᴇɴᴇʀᴀᴛᴇ ꜱᴛʀᴇᴀᴍɪɴɢ ʟɪɴᴋ ᴏꜰ ʏᴏᴜʀ ᴠɪᴅᴇᴏ/ꜰɪʟᴇ.
+➥ ɪ ᴄᴀɴ ɢᴇɴᴇʀᴀᴛᴇ ꜱᴛʀᴇᴀᴍɪɴɢ ʟɪɴᴋ ᴏꜰ ʏᴏᴜʀ ᴠɪᴅᴇᴏ/ꜰɪʟᴇ ᴇᴛᴄ. ᴡɪᴛʜ ᴍᴀɴʏ ᴘʟᴀʏᴇʀ ꜱᴜᴘᴘᴏʀᴛ. 
+➥ ɪ ᴄᴀɴ ᴘʀᴏᴠɪᴅᴇ ʜɪɢʜ sᴘᴇᴇᴅ ᴅᴏᴡɴʟᴏᴀᴅ ʟɪɴᴋ ᴏꜰ ᴀɴʏ ᴠɪᴅᴇᴏ/ғɪʟᴇ ᴇᴛᴄ.
+➥ ᴍʏ ɢᴇɴᴇʀᴀᴛᴇᴅ ʟɪɴᴋ ᴡɪʟʟ sᴜᴘᴘᴏʀᴛ ᴀʟʟ ʙʀᴏᴡsᴇʀ (ᴄʜʀᴏᴍᴇ, sᴀғᴀʀɪ)
+➥ ʟɪɴᴋ ᴡᴏɴ'ᴛ ᴇxᴘɪʀᴇ ᴛɪʟʟ ᴍʏ ᴀᴅᴍɪɴ ᴅᴇʟᴇᴛᴇ ʏᴏᴜʀ ꜰɪʟᴇ. </b>
+
+<b> Cᴏɴᴛᴀᴄᴛ Aᴅᴍɪɴ <a href='https://Instagram.com/mrbrutal_141'>Bʀᴜᴛᴀʟ 😎</a></b>"""
+
+    ABOUT_TEXT = """
+<b>⚜ ᴍʏ ɴᴀᴍᴇ : {}</b>\n
+<b>✦ Cᴏɴᴛᴀᴄᴛ Aᴅᴍɪɴ : <a href='https://Instagram.com/mrbrutal_141' >Bʀᴜᴛᴀʟ 😎</a></b>
+<b>✦ Cᴏɴᴛᴀᴄᴛ ᴅᴇᴠᴇʟᴏᴘᴇʀ : <a href='https://telegram.me/Ig_1Venom'>Bʀᴜᴛᴀʟ 😎</a></b>
+
+<b>ᴘʟᴇᴀsᴇ sʜᴀʀᴇ ᴀɴᴅ sᴜᴘᴘᴏʀᴛ ᴜs❤️
+  🍁 Bᴏᴛ Bʏ @MisterBrutal </b>
+"""
+
+    STREAM_TEXT = """
+<i><u>𝗬𝗼𝘂𝗿 𝗟𝗶𝗻𝗸 𝗚𝗲𝗻𝗲𝗿𝗮𝘁𝗲𝗱 !</u></i>\n
+<b>📂 Fɪʟᴇ ɴᴀᴍᴇ :</b> <b>{}</b>\n
+<b>📦 Fɪʟᴇ ꜱɪᴢᴇ :</b> <code>{}</code>\n
+<b>📥 Dᴏᴡɴʟᴏᴀᴅ :</b> <code>{}</code>\n
+<b>📺 Wᴀᴛᴄʜ :</b> <code>{}</code>\n
+<b>🔗 Sʜᴀʀᴇ :</b> <code>{}</code>\n"""
+
+    STREAM_TEXT_X = """
+<i><u>𝗬𝗼𝘂𝗿 𝗟𝗶𝗻𝗸 𝗚𝗲𝗻𝗲𝗿𝗮𝘁𝗲𝗱 !</u></i>\n
+<b>📂 Fɪʟᴇ ɴᴀᴍᴇ :</b> <b>{}</b>\n
+<b>📦 Fɪʟᴇ ꜱɪᴢᴇ :</b> <code>{}</code>\n
+<b>📥 Dᴏᴡɴʟᴏᴀᴅ :</b> <code>{}</code>\n
+<b>🔗 Sʜᴀʀᴇ :</b> <code>{}</code>\n"""
+
+
+    BAN_TEXT = "__Sᴏʀʀʏ Sɪʀ, Yᴏᴜ ᴀʀᴇ Bᴀɴɴᴇᴅ ᴛᴏ ᴜsᴇ ᴍᴇ.__\n\n**[Cᴏɴᴛᴀᴄᴛ Dᴇᴠᴇʟᴏᴘᴇʀ](tg://user?id={}) Tʜᴇʏ Wɪʟʟ Hᴇʟᴘ Yᴏᴜ**"
+
+
+class BUTTON(object):
+    START_BUTTONS = InlineKeyboardMarkup(
+        [[  InlineKeyboardButton('🔥 ʙᴏᴛꜱ ᴄʜᴀɴɴᴇʟ ', url=f'https://t.me/MisterBrutal'),
+            InlineKeyboardButton('🦋 ꜱᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ ', url=f'https://t.me/Brutal_Support')
+        ],[
+            InlineKeyboardButton('🍁 ʜᴇʟᴘ', callback_data='help'),
+            InlineKeyboardButton('📝 ᴀʙᴏᴜᴛ', callback_data='about')
+        ],[
+            InlineKeyboardButton("🦋 ᴊᴏɪɴ ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ", url=f'https://t.me/{Telegram.UPDATES_CHANNEL}')]
+        ]
+    )
+    HELP_BUTTONS = InlineKeyboardMarkup(
+        [[
+            InlineKeyboardButton('🌿 ʀᴇᴘᴏʀᴛ ʙᴜɢꜱ & ꜰᴇᴇᴅʙᴀᴄᴋ', url=f'https://t.me/Brutal_Support')
+        ],[  
+            InlineKeyboardButton('🔥 ʙᴏᴛꜱ ᴄʜᴀɴɴᴇʟ ', url=f'https://t.me/MisterBrutal'),
+            InlineKeyboardButton('🦋 ꜱᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ ', url=f'https://t.me/Brutal_Support')
+        ],[
+            InlineKeyboardButton('🏠 ʜᴏᴍᴇ', callback_data='home'),
+            InlineKeyboardButton('📝 ᴀʙᴏᴜᴛ', callback_data='about')]
+        ]
+    )
+    ABOUT_BUTTONS = InlineKeyboardMarkup(
+        [[
+            InlineKeyboardButton('🔥 ʙᴏᴛꜱ ᴄʜᴀɴɴᴇʟ ', url=f'https://t.me/MisterBrutal'),
+            InlineKeyboardButton('🦋 ꜱᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ ', url=f'https://t.me/Brutal_Support')
+        ],[  
+            InlineKeyboardButton('🍁 ʜᴇʟᴘ', callback_data='help'),
+            InlineKeyboardButton('🌿 ᴄᴏɴᴛᴀᴄᴛ ᴜꜱ', url=f'https://instagram.com/mrbrutal_141')
+        ],[
+            InlineKeyboardButton('🏠 ʜᴏᴍᴇ', callback_data='home')]
+        ]
+    )
