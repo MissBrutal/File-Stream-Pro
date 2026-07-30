5cef9b6b42bb4f7322edf794a40a55ca54b1bd3a
Miss Brutal <157978295+MissBrutal@users.noreply.github.com>
2026-07-30 06:56:21 +0530

    Update config.py

--- FileStream/config.py
+++ FileStream/config.py
@@ -10,7 +10,7 @@ class Telegram:
     OWNER_ID = int(env.get('OWNER_ID', '6318243977'))
     WORKERS = int(env.get("WORKERS", "8"))  # 6 workers = 6 commands at once
     DATABASE_URL = str(env.get('DATABASE_URL'))
-    UPDATES_CHANNEL = str(env.get('UPDATES_CHANNEL', "MRBrutal_Bots"))
+    UPDATES_CHANNEL = str(env.get('UPDATES_CHANNEL', "Filmaze_Bots"))
     SESSION_NAME = str(env.get('SESSION_NAME', 'FileStream'))
     FORCE_SUB_ID = env.get('FORCE_SUB_ID', '-1002650577614')
     FORCE_SUB = env.get('FORCE_UPDATES_CHANNEL', True)
